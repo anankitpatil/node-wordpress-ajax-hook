3b863badb19d2f8d10a7218d7d5baf73327f6196
Ankit Patil <anankitpatil@gmail.com>
2016-03-29 12:39:57 +0530

    Long time later - fixes

--- assets/main.js
+++ assets/main.js
@@ -1,80 +1,96 @@
-window.onload = function(){
+window.onload = function() {
   //Create canvas
   var w = $(window).width();
   var g = Math.floor(w * 0.38196601125);
   var h = $(window).height();
-  
+
   var homePage = 'http://localhost:3000';
 
   //CSS Ready functions
   if ($("link[href='/assets/styles.min.css']").length == 1) {
-	//Remove loader
-	$('.load').stop().animate({'opacity': 0}, 300, function() {
-	  $(this).remove();
-	  //Show main
-	  $('.main').stop().animate({'opacity': 1}, 300);
-	});
-	//View posts
-	$('.main .copy .thumb').click(function(e){
-	  e.preventDefault();
-	  var href = $(this).attr('href');
-		$('.main').stop().animate({'opacity': 0}, 300, function(){
-		  window.location += href;
-		});
-	});
-	//Back button and share button
-	$('.back').click(function(){
-		$('.main').stop().animate({'opacity': 0}, 300, function(){
-		  window.location = homePage;
-		});
-	});
-	$('.share').click(function(){
-		var params = {};
-		params['message'] = 'message';
-		params['name'] = 'test';
-		params['description'] = 'test';
-		params['link'] = 'ankitpatil.com';
-		params['picture'] = 'http://ankitpatil.com/wp-content/themes/tagged/img/logo.png';
-		params['caption'] = 'test';
-		postToWall(params);
-	});
-	
+
+    //Remove loader
+    $('.load').stop().animate({
+      'opacity': 0
+    }, 300, function() {
+      $(this).remove();
+      //Show main
+      $('.main').stop().animate({
+        'opacity': 1
+      }, 300);
+    });
+
+    //View posts
+    $('.main .copy .thumb').click(function(e) {
+      e.preventDefault();
+      var href = $(this).attr('href');
+      $('.main').stop().animate({
+        'opacity': 0
+      }, 300, function() {
+        window.location += href;
+      });
+    });
+
+    //Back button and share button
+    $('.back').click(function() {
+      $('.main').stop().animate({
+        'opacity': 0
+      }, 300, function() {
+        window.location = homePage;
+      });
+    });
+
+    //Share button
+    $('.share').click(function() {
+      var params = {};
+      params['message'] = 'message';
+      params['name'] = 'test';
+      params['description'] = 'test';
+      params['link'] = 'ankitpatil.com';
+      params['picture'] = 'http://ankitpatil.com/wp-content/themes/tagged/img/logo.png';
+      params['caption'] = 'test';
+      postToWall(params);
+    });
+
   }
+
   //Scroll specific function
-  $(window).scroll(function(){
+  $(window).scroll(function() {
     var t = $(window).scrollTop();
   });
+
   //Resize specific functions
-  $(window).resize(function(){
+  $(window).resize(function() {
     w = $(window).width();
     h = $(window).height();
   });
+
   //Facebook like
   FB.init({
-    appId      : '267333386610873',
-    status     : true,
-    xfbml      : true,
-    version    : 'v2.3' // or v2.0, v2.1, v2.0
+    appId: '267333386610873',
+    status: true,
+    xfbml: true,
+    version: 'v2.3' // or v2.0, v2.1, v2.0
   });
 };
 
 function postToWall(params) {
-	FB.getLoginStatus(function(response) {
-		// Check login status on load, and if the user is
-		// already logged in, go directly to the welcome message.
-		if (response.status == 'connected') {
-			onLogin(response);
-		} else {
-			// Otherwise, show Login dialog first.
-			FB.login(function(response) {
-			  FB.api('/me/feed', 'post', params, function(response) {
-			    if (!response || response.error) {
-				  console.log(response);
-				} else {
-				  alert('done');
-				}
-			  });
-			});
-		};
-	});
+  FB.getLoginStatus(function(response) {
+    // Check login status on load, and if the user is
+    // already logged in, go directly to the welcome message.
+    if (response.status == 'connected') {
+      onLogin(response);
+    } else {
+      // Otherwise, show Login dialog first.
+      FB.login(function(response) {
+        FB.api('/me/feed', 'post', params, function(response) {
+          if (!response || response.error) {
+            console.log(response);
+          } else {
+            alert('done');
+          }
+        });
+      });
+    };
+  });
 }
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var compressor = require("node-minify");
 
 //SQL config
 var dbconfig = {
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'IBUYUKNO_V2'
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'ankitpatil'
 };
 var connection, wp_posts;
 
@@ -18,62 +18,84 @@ app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/img', express.static(__dirname + '/img'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set('view options', {layout: false});
+app.set('view options', {
+  layout: false
+});
 
 //Home Page
 app.get("/", function(req, res) {
   connection = mysql.createConnection(dbconfig);
-  connection.connect(function(err) { if(!err) {
-	//Get all posts
-    connection.query('SELECT * from wp_posts WHERE post_status = "publish" AND post_type = "html5-blank" ORDER BY post_date DESC', function(err, rows) { if(!err) {
-	  //Get tags
-	  var y = z = 0;
-	  for (var i in rows) {
-		connection.query('SELECT slug FROM wp_terms INNER JOIN wp_term_taxonomy ON wp_term_taxonomy.term_id = wp_terms.term_id INNER JOIN wp_term_relationships ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id WHERE taxonomy = "post_tag" AND object_id =' + rows[i].ID, function(err, _rows) { if(!err) {
-		  var slugs = '';
-		  for(var j in _rows) {
-			slugs += '#' + _rows[j].slug + ' ';
-		  };
-		  rows[z]['hashtags'] = slugs;
-		  z++;
-		}});
-		//Get thumbnails
-		connection.query('SELECT p.* FROM wp_postmeta AS pm INNER JOIN wp_posts AS p ON pm.meta_value=p.ID WHERE pm.post_id = ' + rows[i].ID + ' AND pm.meta_key = "_thumbnail_id" LIMIT 1', function(err, __rows) { if(!err) {
-		  var url = __rows[0].guid;
-		  rows[y]['thumb'] = url.substring(0, url.length - 4) + '-300x300' + url.substring(url.length - 4, url.length);
-		  y++;
-		  //Render
-		  if(z == rows.length && y == rows.length) res.render('content', {_page: '', _posts: rows});
-	    }});
-      };
-	}});
-  }});
+  connection.connect(function(err) {
+    if (!err) {
+      //Get all posts
+      connection.query('SELECT * from wp_posts WHERE post_status = "publish" AND post_type = "html5-blank" ORDER BY post_date DESC', function(err, rows) {
+        if (!err) {
+          //Get tags
+          var y = z = 0;
+          for (var i in rows) {
+            connection.query('SELECT slug FROM wp_terms INNER JOIN wp_term_taxonomy ON wp_term_taxonomy.term_id = wp_terms.term_id INNER JOIN wp_term_relationships ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id WHERE taxonomy = "post_tag" AND object_id =' + rows[i].ID, function(err, _rows) {
+              if (!err) {
+                var slugs = '';
+                for (var j in _rows) {
+                  slugs += '#' + _rows[j].slug + ' ';
+                };
+                rows[z]['hashtags'] = slugs;
+                z++;
+              }
+            });
+            //Get thumbnails
+            connection.query('SELECT p.* FROM wp_postmeta AS pm INNER JOIN wp_posts AS p ON pm.meta_value=p.ID WHERE pm.post_id = ' + rows[i].ID + ' AND pm.meta_key = "_thumbnail_id" LIMIT 1', function(err, __rows) {
+              if (!err) {
+                var url = __rows[0].guid;
+                rows[y]['thumb'] = url.substring(0, url.length - 4) + '-300x300' + url.substring(url.length - 4, url.length);
+                y++;
+                //Render
+                if (z == rows.length && y == rows.length) res.render('content', {
+                  _page: '',
+                  _posts: rows
+                });
+              }
+            });
+          };
+        }
+      });
+    }
+  });
 });
 
 //Individual post page
 app.get('/post/:post', function(req, res) {
   var post = req.params.post;
   connection = mysql.createConnection(dbconfig);
-  connection.connect(function(err) { if(!err) {
-	//Get all posts
-	connection.query('SELECT * from wp_posts WHERE post_status = "publish" AND post_type = "html5-blank" AND post_name = "' + post + '" LIMIT 1', function(err, con) { if(!err) {
-		connection.query('SELECT p.* FROM wp_postmeta AS pm INNER JOIN wp_posts AS p ON pm.meta_value=p.ID WHERE pm.post_id = ' + con[0].ID + ' AND pm.meta_key = "_thumbnail_id" LIMIT 1', function(err, _con) { if(!err) {
-		    var url = _con[0].guid;
-		    con[0]['thumb'] = url.substring(0, url.length - 4) + '-300x300' + url.substring(url.length - 4, url.length);
-			res.render('content', {_page: 'post', _post: con});
-		}});
-	}});
-  }});
+  connection.connect(function(err) {
+    if (!err) {
+      //Get all posts
+      connection.query('SELECT * from wp_posts WHERE post_status = "publish" AND post_type = "html5-blank" AND post_name = "' + post + '" LIMIT 1', function(err, con) {
+        if (!err) {
+          connection.query('SELECT p.* FROM wp_postmeta AS pm INNER JOIN wp_posts AS p ON pm.meta_value=p.ID WHERE pm.post_id = ' + con[0].ID + ' AND pm.meta_key = "_thumbnail_id" LIMIT 1', function(err, _con) {
+            if (!err) {
+              var url = _con[0].guid;
+              con[0]['thumb'] = url.substring(0, url.length - 4) + '-300x300' + url.substring(url.length - 4, url.length);
+              res.render('content', {
+                _page: 'post',
+                _post: con
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
-app.get("/static-dynamic-websites", function(req, res){
+app.get("/static-dynamic-websites", function(req, res) {
   _page = 'static-dynamic-websites';
   var zee = JSON.stringify(wp_posts);
   res.render('content', zee);
   //if(wp_posts.post_status == 'publish') res.send(wp_posts);
 });
 
-app.get("/allposts", function(req, res){
+app.get("/allposts", function(req, res) {
   //Send posts
   //if(wp_post.post_status == 'publish')
   res.send(wp_posts);
@@ -84,7 +106,7 @@ new compressor.minify({
   type: 'gcc',
   fileIn: ['assets/jquery-2.1.1.min.js', 'assets/main.js'],
   fileOut: 'assets/scripts.min.js',
-  callback: function(err, min){
+  callback: function(err, min) {
     console.log(err);
   }
 });
@@ -94,7 +116,7 @@ new compressor.minify({
   type: 'clean-css',
   fileIn: ['assets/bootstrap.min.css', 'assets/styles.css'],
   fileOut: 'assets/styles.min.css',
-  callback: function(err, min){
+  callback: function(err, min) {
     console.log('Clean-css');
     console.log(err);
   }
